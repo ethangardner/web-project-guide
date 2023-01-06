5b070636c3ed0e04bdac0d9db9384314fa99bfc0
Ethan Gardner <egardner@taunton.com>
2023-01-06 15:30:06 -0500

    improve build process

---
+++ spack.config.js
@@ -0,0 +1,26 @@
+const { config } = require("@swc/core/spack");
+const path = require("path");
+
+module.exports = config({
+  entry: {
+    build: path.join(__dirname, "js/main.ts"),
+  },
+
+  output: {
+    path: path.join(__dirname, "js"),
+    name: "main.js",
+  },
+
+  options: {
+    jsc: {
+      target: "es2016",
+      parser: {
+        syntax: "typescript",
+      },
+    },
+    env: {
+      // path to package.json which includes browserslist field
+      path: ".",
+    },
+  },
+});
